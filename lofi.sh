fb1a60da68915cd762116d1a6d21346eccadb3eb
Joel Rodrigues <45329484+pantsufan@users.noreply.github.com>
2021-06-24 09:10:00 +0530

    Add files via upload

---
+++ lofi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+url="https://youtu.be/7NOSDKb0HlU"
+mpv "$url" --no-video
---
+++ wallup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/joel/Downloads/walls
+git add .
+git commit -m "New Wallpaper Added"
+git push
+echo "Done"
